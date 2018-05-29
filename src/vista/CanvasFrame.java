500c3a497d660edec42f8ed5c8475780395b5e0d
alu604 <alu604@604PC12.labs>
2018-05-28 21:46:06 -0300

    nuevo package

---
+++ src/vista/CanvasFrame.java
@@ -0,0 +1,19 @@
+package vista;
+
+public class CanvasFrame {
+
+	public CanvasFrame(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void configurar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void presentar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
